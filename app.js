addb9d6098455bcc62d7864aade69406c4dd449e
klinkzsweet <klinkzsweet@gmail.com>
2023-11-30 12:51:22 +0700

    membuat tabel user

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const app = express()
 const PORT = 7000
+const routes = require("./routes")
 
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(routes)
 
 app.get("",  (req, res) => {
     res.send("FP3")
