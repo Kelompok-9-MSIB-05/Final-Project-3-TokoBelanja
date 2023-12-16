9106cad0f9148d6c92c432d228ae4bddddfe9447
Ketut Satya Krsna Cakra Buana <Ketutkrsna@gmail.com>
2023-12-16 22:03:54 +0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRoutes = require('./UserRoutes');
 const categoryRoutes = require('./CategoryRoutes');
 const productRoutes = require('./productRoutes');
+const transactionRoutes = require('./transactionRoutes');
 
 
 const router = express.Router();
@@ -9,6 +10,7 @@ const router = express.Router();
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
+router.use('/transactions', transactionRoutes);
 
 
 module.exports = router;
